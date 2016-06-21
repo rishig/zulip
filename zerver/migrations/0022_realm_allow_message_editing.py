fad6a64417014bd2bcd63d273f6a56fd001e5f60
Rishi Gupta <rishig@kandralabs.com>
2016-06-21 12:34:41 -0700

    Add realm setting to disable message editing.
    This is controlled through the admin tab and a new field in the Realms table.
    This mirrors the behavior of feature_flags.disable_message_editing.
    Partially resolves Issue 903.

---
+++ zerver/migrations/0022_realm_allow_message_editing.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('zerver', '0021_migrate_attachment_data'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='realm',
+            name='allow_message_editing',
+            field=models.BooleanField(default=True),
+        ),
+    ]
