95a75772f6048ce73aa5631b4c48eecc40c39593
Rishi Gupta <rishig@kandralabs.com>
2016-08-14 01:01:44 -0700

    Refactor integrations.py using zerver.lib.utils.get_subdomain

--- zerver/views/integrations.py
+++ zerver/views/integrations.py
@@ -3,16 +3,7 @@ from django.views.generic import TemplateView
 from django.conf import settings
 
 from zerver.lib.integrations import INTEGRATIONS
-
-# TODO: write this properly, and put it somewhere reusable
-def host_is_realm_uri(host):
-    if host.split(':')[0] in ('zulipchat.com', 'localhost', 'zulipdev.com'):
-        return False
-    return True
-
-# TODO also do this properly
-def get_realm_from_host(host):
-    return host.split('.')[0]
+from zerver.lib.utils import get_subdomain
 
 class IntegrationView(TemplateView):
     template_name = 'zerver/integrations.html'
@@ -21,17 +12,17 @@ class IntegrationView(TemplateView):
         context = super(IntegrationView, self).get_context_data(**kwargs)
         alphabetical_sorted_integration = OrderedDict(sorted(INTEGRATIONS.items()))
         context['integrations_dict'] = alphabetical_sorted_integration
-        host = self.request.get_host()
-        if host_is_realm_uri(host):
+        subdomain = get_subdomain(self.request)
+        if subdomain:
             settings_html = '<a href="/#settings">Zulip settings page</a>'
             subscriptions_html = '<a target="_blank" href="../#subscriptions">subscriptions page</a>'
-            realm_str = get_realm_from_host(host)
+            subdomain_ = subdomain
         else:
             settings_html = 'Zulip settings page'
             subscriptions_html = 'subscriptions page'
-            realm_str = 'yourZulipDomain'
+            subdomain_ = 'yourZulipDomain'
 
-        external_api_path_subdomain = '%s.%s' % (realm_str, settings.EXTERNAL_API_PATH)
+        external_api_path_subdomain = '%s.%s' % (subdomain_, settings.EXTERNAL_API_PATH)
         external_api_uri_subdomain = '%s%s' % (settings.EXTERNAL_URI_SCHEME,
                                                external_api_path_subdomain)
 
